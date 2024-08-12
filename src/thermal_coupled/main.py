8c8182e6e724d0acc6cd80d71b4185b5f37448a9
Laminar_Boh <kevinpfau17@gmail.com>
2024-08-12 15:56:56 -0400

    test problem updates

--- src/thermal_coupled/main.py
+++ src/thermal_coupled/main.py
@@ -24,28 +24,34 @@ from thermal_coupled.therm_dist import build_model
 
 
 # specify number of components and data file name
-n = 5
-data_file_name = '5_comp_hydrocarbon.xlsx'
+n = 3
+data_file_name = '3_comp_test.xlsx'
 
 # import problem data for system and relevant species to data object
-hydrocarbon_data = Data(data_file_name)
+mixture_data = Data(data_file_name)
 
 # build state-task network superstrucutre and associated index sets
 network_superstructure = stn(n)
 network_superstructure.generate_tree()
 network_superstructure.generate_index_sets()
 
-model = build_model(network_superstructure, hydrocarbon_data)
+model = build_model(network_superstructure, mixture_data)
+
+print()
+print('Inlet data')
+print('================================================================')
+print(mixture_data.system_df)
+
+print()
+print('Mixture species data')
+print('================================================================')
+print(mixture_data.species_df)
 
 print()
 print(f'Model type before transformation: {get_model_type(model)}')
 
-print()
-print('Model size before transformation:')
-print(build_model_size_report(model))
-
-# uncomment below line to save the Pyomo model to a txt file to examine model
-# save_model_to_file(model, '3_comp_pyomo_model')
+# saving the pyomo model to a file
+save_model_to_file(model, '3_comp_model')
 
 # SOLUTION
 # ================================================
@@ -62,47 +68,43 @@ print(f'Model type after transformation: {get_model_type(model)}')
 # MODEL ANALYSIS
 # =================================================================
 
-# print('Model size after transformation:')
-# print(build_model_size_report(model))
+print()
+print('Model size after transformation:')
+print(build_model_size_report(model))
 
-# print()
-# # print(f'Model type after transformation: {get_model_type(model)}')
-
-# solver = pyo.SolverFactory('gurobi')
+# solving model
+solver = pyo.SolverFactory('gams:baron')
 
 # # Gurobi solver options
 # solver.options = {'NumericFocus': 2,
 #                   'nonConvex': 2}
 
-# results = solver.solve(model, tee=True)
+results = solver.solve(model, tee=True)
 
-# # uncomment below line if you want to see solver results, problem size
-# print(results)
+# Log infeasible constraints if any
+logging.basicConfig(level=logging.INFO)
+log_infeasible_constraints(model)
+find_infeasible_constraints(model)
 
-# # Log infeasible constraints if any
-# logging.basicConfig(level=logging.INFO)
-# log_infeasible_constraints(model)
-# find_infeasible_constraints(model)
+# SOLUTION OUTPUT
+# =================================================================
+pprint_network(model)
 
+# uncomment below line to save the solution output to a txt file
+# save_solution_to_file(model, '4_comp_solution')
 
-# # SOLUTION OUTPUT
-# # =================================================================
-# pprint_network(model)
+# save_model_to_file(model, '3_comp_pyomo_model_solution')
 
-# # # uncomment below line to save the solution output to a txt file
-# # # save_solution_to_file(model, '4_comp_solution')
+print()
 
-# # # save_model_to_file(model, '3_comp_pyomo_model_solution')
+for i in model.COMP:
+    print(f'Final heat exchanger active: {i} {pyo.value(model.final_heat_exchanger[i].indicator_var)}')
 
-# print()
+print()
+for s in model.ISTATE:
+    print(f'Intermediate heat exchanger active: {s} {pyo.value(model.int_heat_exchanger[s].indicator_var)}')
 
-# for i in model.COMP:
-#     print(f'Final heat exchanger active: {i} {pyo.value(model.final_heat_exchanger[i].indicator_var)}')
-
-# print()
-# for s in model.ISTATE:
-#     print(f'Intermediate heat exchanger active: {s} {pyo.value(model.int_heat_exchanger[s].indicator_var)}')
-
-# print()
-# for k in model.TASKS:
-#     print(f'Task ({k}): {pyo.value(model.column[k].indicator_var)}')
+print()
+for k in model.TASKS:
+    print(f'Task ({k}): {pyo.value(model.column[k].indicator_var)}')
+    
